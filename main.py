4b0a8914e14d11069358eb9f058c087cbfe0b757
YLu-1258 <maodou1258@gmail.com>
2022-09-13 13:44:48 -0700

    fixed about_me page

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def page_not_found(e):
 def index():
     return render_template("index.html")
 
-@app.route('/stub/')  # connects /stub/ URL to stub() function
+@app.route('/stub/')  # connects about_me.html URL to stub() function
 def stub():
-    return render_template("stub.html")
+    return render_template("about_me.html")
 
 # this runs the application on the development server
 if __name__ == "__main__":
