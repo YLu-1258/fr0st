5b286d8412617ff0b8c6427d0b63ceea5e555ba6
YLu-1258 <maodou1258@gmail.com>
2022-10-28 13:04:15 -0700

    base64 coments

--- base_64.py
+++ base_64.py
@@ -1,8 +1,11 @@
 import base64
 
+# WE use ascii encoding here WOOOHOOO
 
+# Encodes the password in question
 def encode(pwd):
     return base64.b64encode(pwd.encode('ascii')).decode('ascii')
 
+# Decodes the pasword in question
 def decode(pwd):
     return base64.b64decode(pwd.encode('ascii')).decode('ascii')
